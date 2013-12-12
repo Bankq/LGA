018f41dce1f0786937a387634212b5120b7e031f
Bank Qian <ibank.qh@gmail.com>
2013-12-12 11:48:37 -0500

    Now test_scanner have the complete token list

--- LGA/tests/test_scanner.ml
+++ LGA/tests/test_scanner.ml
@@ -5,7 +5,8 @@ open Utils
 
 let _ = 
   let lexbuf = Lexing.from_channel (open_in "test_scanner_in.lga") in 
-  let token_list = token_list_of_lexbuf lexbuf Scanner.token Parser.EOF in
+  let raw_token_list = token_list_of_lexbuf lexbuf Scanner.token Parser.EOF in
+  let token_list = expand_token_list raw_token_list in
   let print x = print_endline (string_of_token x) in
   List.iter print token_list
 
